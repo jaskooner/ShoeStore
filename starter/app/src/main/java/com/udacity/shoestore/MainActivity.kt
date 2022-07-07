5158a5382dda6be85a19a86df8981904b0f069d5
jaskooner <jaskooner.singh@gmail.com>
2022-07-07 13:41:09 +0100

    set up login ui as main activity

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
 
         //binding
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
